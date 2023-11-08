1a493b51f8d0a4cb604bb4d82ec37a8554547146
Ayshen Salahova <TU81UEBFJ@code.edu.az>
2023-11-09 00:37:49 +0400

    updated site

--- movie_website/assets/js/script.js
+++ movie_website/assets/js/script.js
@@ -4,8 +4,9 @@ var row = document.getElementsByClassName("movies")[0]
 
 function FillHtml(data) {
     data.forEach(movie => {
-        row.innerHTML += `
-        <div class="card col-3 m-2" style="width: 18rem;">
+        row.innerHTML += 
+        `
+        <div class="card col-3 m-2" style="width: 17rem;">
         <img class="img pt-1" src="${movie.image.medium}" class="card-img-top" alt="${movie.name}">
         <div class="card-body">
           <h5 class="card-title fw-bold">${movie.name}</h5>
